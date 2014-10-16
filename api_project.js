8e610a1292930a1d6c229e47b503fdd2e50bb1a8
Matthias Lill <m.lill@gmx.de>
2014-10-16 12:41:44 +0200

    SSL API support, correct DEV-Adress

--- api_project.js
+++ api_project.js
@@ -3,15 +3,15 @@ define({
   "version": "1.0.0",
   "description": "Anbindung an das Lockbox LTS.",
   "title": "Lockbox RESTful API V1",
-  "url": "http://api.lockboxsystem.com/v1",
+  "url": "https://api.lockboxsystem.com/v1",
   "header": {
-    "content": "<p>Die Lockbox API ermöglicht es Informationen zu Kunden und Sendungen direkt und einfach von <a href=\"http://www.lockboxsystem.com\">Lockbox</a> abzufragen.</p>\n<h2 id=\"authentifizierung\">Authentifizierung</h2>\n<p>Zu jeder Anfrage an unsere API muss der API-Key aus dem ServiceProvider-Konto als <code>GET</code>-Parameter mit gesendet werden. Bitte halte diesen API-Key stets geheim und vertraulich.</p>\n<p>Beispiel</p>\n<pre><code>http://api.lockboxsystem.com/v1/customer/anchor/a123?apikey=abcdef....\n</code></pre><h2 id=\"testen\">Testen</h2>\n<p>Zum Testen einer Funktion kannst du diese an eine alternative Basisadresse absenden. Nutze dafür die Adresse <code>http://apitest.lockboxsystem.com/v1</code> zusammen mit dem normalen API-Key. Alle Anfragen an dieses System werden regulär verarbeitet aber erstellte Sendungen ggf. nach einer unbestimmten Zeit wieder verworfen.</p>\n<h2 id=\"versionierung\">Versionierung</h2>\n<p>Neuerungen an der API sind nicht ausgeschlossen. Dabei wird stets darauf geachtet, dass vorherige Versionen weiterhin ohne Beeinträchtigung funktionieren. Diese Dokumentation ist auf GitHub (<a href=\"https://github.com/lockboxsystem/apidoc\">https://github.com/lockboxsystem/apidoc</a>) verfügbar und für Benachrichtigungen über Veränderungen verfolgt werden.</p>\n"
+    "content": "<p>Die Lockbox API ermöglicht es Informationen zu Kunden und Sendungen direkt und einfach von <a href=\"http://www.lockboxsystem.com\">Lockbox</a> abzufragen.</p>\n<h2 id=\"authentifizierung\">Authentifizierung</h2>\n<p>Zu jeder Anfrage an unsere API muss der API-Key aus dem ServiceProvider-Konto als <code>GET</code>-Parameter mit gesendet werden. Bitte halte diesen API-Key stets geheim und vertraulich.</p>\n<p>Beispiel</p>\n<pre><code>https://api.lockboxsystem.com/v1/customer/anchor/a123?apikey=abcdef....\n</code></pre><h2 id=\"testen\">Testen</h2>\n<p>Zum Testen einer Funktion kannst du diese an eine alternative Basisadresse absenden. Nutze dafür die Adresse <code>http://api.lckbx.de/v1</code> zusammen mit dem normalen API-Key. Alle Anfragen an dieses System werden regulär verarbeitet aber erstellte Sendungen ggf. nach einer unbestimmten Zeit wieder verworfen.</p>\n<h2 id=\"versionierung\">Versionierung</h2>\n<p>Neuerungen an der API sind nicht ausgeschlossen. Dabei wird stets darauf geachtet, dass vorherige Versionen weiterhin ohne Beeinträchtigung funktionieren. Diese Dokumentation ist auf GitHub (<a href=\"https://github.com/lockboxsystem/apidoc\">https://github.com/lockboxsystem/apidoc</a>) verfügbar und für Benachrichtigungen über Veränderungen verfolgt werden.</p>\n"
   },
   "footer": {
     "content": "<h2 id=\"weiterf-hrende-links\">Weiterführende Links</h2>\n<p>Die API folgt weitesgehend den Konventionen von <a href=\"http://jsonapi.org\">http://jsonapi.org</a> zur einheitlichen Gestaltung der Anfragen und Antworten.</p>\n"
   },
   "generator": {
     "version": "0.7.2",
-    "time": "2014-10-15T08:28:58.490Z"
+    "time": "2014-10-16T10:38:53.862Z"
   }
 });
